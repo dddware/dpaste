7387ca33a70f82dcdf8efa598fdf74a7d1bc8430
Tom Panier <tpanier@auchan.net>
2016-03-30 13:12:30 +0200

    fixed domain and own blog URL and title and enchanced code syntax

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 // Scaffolding
-var express = require('express');
-var mongoose = require('mongoose');
-var app = express();
+var express = require('express'),
+    mongoose = require('mongoose'),
+    app = express();
+
 mongoose.connect('localhost', 'dpaste');
 
 // Configuration
-app.configure(function()
-{
+app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.set('views', __dirname + '/views');
     app.use(require('stylus').middleware({ src: __dirname + '/public' }));
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
-    app.domain = 'dpaste.cc';
     app.port = 3001;
 });
 
@@ -21,28 +20,26 @@ app.configure(function()
 var Paste = mongoose.model('Paste', mongoose.Schema({ paste: 'string', created_at: 'string', fruit: 'number' }));
 
 // Middleware
-app.all('*', function(req, res, next)
-{
+app.all('*', function(req, res, next) {
     // Set base URL for templates
-    app.locals.base = req.protocol + '://' + app.domain;
+    app.locals.base = 'https://dpaste.neemzy.org';
     next();
 });
 
 // Routes
-app.get('/', function(req, res)
-{
+app.get('/', function(req, res) {
     res.render('index.jade', { title: 'dpaste', active: 'active' });
 });
 
-app.get('/paste/:id', function(req, res)
-{
-    Paste.findById(req.params.id, function(err, paste)
-    {
-        if (err)
+app.get('/paste/:id', function(req, res) {
+    Paste.findById(req.params.id, function(err, paste) {
+        if (err) {
             console.log(err);
+        }
 
-        if (req.headers['user-agent'].match(/curl/))
+        if (req.headers['user-agent'].match(/curl/)) {
             res.send(200, paste.paste);
+        }
 
         res.render('index.jade', {
             title: 'dpaste - ' + paste.id,
@@ -55,32 +52,30 @@ app.get('/paste/:id', function(req, res)
     });
 });
 
-app.post('/paste/new', function(req, res)
-{
-    if (lepaste = req.param('paste'))
-    {
+app.post('/paste/new', function(req, res) {
+    if (lePaste = req.param('paste')) {
         var paste = new Paste({
-            paste: lepaste,
+            paste: lePaste,
             created_at: new Date().getTime(),
             fruit: Math.floor(Math.random() * 9)
         });
 
-        paste.save(function(err)
-        {
-            if (err)
+        paste.save(function(err) {
+            if (err) {
                 console.log(err);
+            }
 
-            var new_paste = '/paste/' + paste.id;
+            var newPaste = '/paste/' + paste.id;
 
-            if (req.headers['user-agent'].match(/curl/))
-                res.send(201, app.locals.base + new_paste);
-            else
-                res.redirect(new_paste);
+            if (req.headers['user-agent'].match(/curl/)) {
+                res.send(201, app.locals.base + newPaste);
+            } else {
+                res.redirect(newPaste);
+            }
         });
-    }
-
-    else
+    } else {
         res.redirect('/');
+    }
 });
 
 app.listen(app.port);
